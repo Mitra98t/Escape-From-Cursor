9795d08d73e500f43db1f37ec11bd6b6df275eb1
Federico Miraglia <51447075+Mitra98t@users.noreply.github.com>
2021-11-03 21:21:06 +0100

    added gsap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { TweenMax, Power3 } from 'gsap'
+
 function App() {
   return (
-    <>
-    
-    </>
+    <div className="w-full min-h-screen h-full">
+    </div>
   );
 }
 
